a315a49016df51f56c112b9afd8e5c786289865e
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-10 01:11:59 +0530

    Components with Fragments

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+function ListGroup() {
+  return (
+    <div>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+      </ul>
+    </div>
+  );
+}
+export default ListGroup;
